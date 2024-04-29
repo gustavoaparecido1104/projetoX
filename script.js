72bb048a74fd7b1b5da2a3e728d3586042a664cb
gustavoaparecido1104 <109361069+gustavoaparecido1104@users.noreply.github.com>
2024-04-29 13:42:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+const prontoBtn = document.querySelector('button[type="submit"]');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
+prontoBtn.addEventListener('click', () => {
+    // Redirecionar para outro site
+    window.location.href = 'inicio.html';
+});
